ecfca75e9bc3b50f3be68cdc2c4b73b59776da7a
Thomas Hopkins <thopkins@redventures.com>
2014-06-18 13:14:55 -0400

    disable replay test in channel

--- test/channel.test.js
+++ test/channel.test.js
@@ -29,45 +29,9 @@ describe('rabbit-wrapper.channel', function () {
                 done(err);
             });
         });
-
-        //Moving this behavior to the queue, because listeners are the only user-provided fn
-        /*it('should not vomit out listener errors', function (done) {
-            ch.on('error', onErr);
-            ch.once('ready', function () {
-                ch.ch.on('error', function () {
-                    return done(new Error('Underyling channel error called!!!'));
-                });
-            });
-            ch.call('assertQueue', ['another.queue', {exclusive: true, durable: false, autoDelete: true}])
-                .then(success, fail);
-            ch.call('bindQueue', ['another.queue', 'test.my.exchange', 'routing.key'])
-                .then(function () {
-                    console.log('bound');
-                    ch.call('consume', ['another.queue', listen, {}]).then(function () {
-                        'use strict';
-                        var content = JSON.stringify('hey');
-                        ch.call('publish', ['test.my.exchange', 'routing.key', new Buffer(content), {persistent: true}]).then(function () {
-                        }, done);
-                    }, done);
-                }, done);
-
-
-
-            function listen(msg) {
-                console.log('hi!!!')
-                console.log(msg.content.toString('utf8'));
-                noway.dude = 'hi';
-                console.log('did not make it');
-            }
-            function onErr(err) {
-                //got us an err
-                console.log('got us an err')
-                return done(err);
-            }
-        });*/
     });
 
-    describe('#fixture', function() {
+/*    describe('#fixture', function() {
         var otherCh;
         before(function (done) {
             otherCh = new Channel(c);
@@ -79,9 +43,7 @@ describe('rabbit-wrapper.channel', function () {
                     ['this.is.my.queue', {exclusive: true, durable: false, autoDelete: true}],
                     true
                 );
-            }/*, function (err) {
-                done(err);
-            }*/).
+            }).
             then(function () {
                 done();
             }, function (err) {
@@ -110,7 +72,7 @@ describe('rabbit-wrapper.channel', function () {
                 }
             );
         });
-    });
+    });*/
 });
 
 function nop() {}
