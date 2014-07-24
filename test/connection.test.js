2c5d6c6a35d8fc14297fa05f654837add81fcd1d
Thomas Hopkins <thopkins@redventures.com>
2014-07-24 15:22:47 -0400

    add disconnection logic

--- test/connection.test.js
+++ test/connection.test.js
@@ -2,16 +2,29 @@ var Rabbit = require('../lib/connection');
 
 describe('node-amqp wrapper', function () {
 
-	describe('#connection', function () {
+	describe('connection', function () {
 		var conn;
 
 		beforeEach(function () {
 			conn = new Rabbit('amqp://localhost:5672');
 
 		});
-
-		it('should connect to rabbit', function (done) {
-			conn.connect(done);
+		describe('#connect', function () {
+			it('should connect to rabbit', function (done) {
+				conn.connect(function () {
+					done();
+					conn.close();
+				});
+			});			
+		});
+		
+		describe('#close', function () {
+			var conn = new Rabbit('amqp://localhost:5672');
+			it('should disconnect', function (done) {
+				conn.connect(function () {
+					conn.close(done);
+				});
+			});
 		});
 	});
 	
